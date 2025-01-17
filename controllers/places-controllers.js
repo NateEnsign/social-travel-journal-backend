ee1b5eda3203e2d956c7ded6117e475ab5fe5077
Nate Ensign <dixienate@gmail.com>
2025-01-16 18:27:35 -0700

    added route and middleware to send post request for adding new place. also added in uuid to create unique ids

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,3 +1,6 @@
+const { v4: uuidv4 } = require('uuid');
+
+
 const HttpError = require("../models/http-error");
 
 const DUMMY_PLACES = [
@@ -43,5 +46,25 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const newId = uuidv4();
+
+  const createdPlace = {
+    id: newId,
+    title: title,
+    description: description,
+    location: coordinates,
+    address: address,
+    creator: creator
+  }
+
+  DUMMY_PLACES.push(createdPlace);
+
+  res.status(201).json({place: createdPlace})
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
