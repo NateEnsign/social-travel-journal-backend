fe89e3e08a51704e7bd1b55aeea6ec1d661ec0c3
Nate Ensign <dixienate@gmail.com>
2025-01-30 19:34:17 -0700

    cleaned a few things up, no major changes

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -7,20 +7,6 @@ const getCoordsForAddress = require("../util/location");
 const Place = require("../models/Place");
 const User = require('../models/user');
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "The tallest building in the world",
-    location: {
-      lat: 40.5555555,
-      lng: -73.9999999,
-    },
-    address: "20 W 34th St, New York, NY 10001",
-    creator: "u1",
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
 
