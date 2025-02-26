d10ab428e417d2f205f1dd5e4434665143cf658c
Nate Ensign <dixienate@gmail.com>
2025-02-25 20:11:20 -0700

    added checks to ensure proper file types are accepted and unsupported filed types are rejected

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-const uuid = require('uuid')
+const { v4: uuidv4 } = require('uuid');
 
 const MIME_TYPE_MAP = {
     'image/png': 'png',
@@ -15,10 +15,14 @@ const fileUpload = multer({
         },
         filename: (req, file, cb) => {
             const ext = MIME_TYPE_MAP[file.mimetype]
-            cb(null, uuid() + '.' + ext);
+            cb(null, uuidv4() + '.' + ext);
         }
     }),
-    fileFilter: 
+    fileFilter: (req, file, cb) => {
+        const isValid = !!MIME_TYPE_MAP[file.mimetype];
+        let error = isValid ? null : new Error('Invalid mime type!');
+        cb(error, isValid);
+    }
 });
 
 module.exports = fileUpload;
