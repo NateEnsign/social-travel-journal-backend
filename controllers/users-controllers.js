c2a540485ef5705621bd4dd09bedb54c75130d5e
Nate Ensign <dixienate@gmail.com>
2025-02-25 20:37:58 -0700

    made changes to now upload image for each user

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -20,6 +20,7 @@ const getUsers = async (req, res, next) => {
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+
     return next(
       new HttpError("Invalid inputs passed, please check your data.", 422)
     );
@@ -46,7 +47,7 @@ const signup = async (req, res, next) => {
   const createdUser = new User({
     name,
     email,
-    image: "https://cdn.nba.com/headshots/nba/latest/1040x760/893.png",
+    image: req.file.path,
     password,
     places: [],
   });
