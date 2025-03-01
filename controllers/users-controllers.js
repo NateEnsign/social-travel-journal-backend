112941203cb245294d6f30e8b8cf3916ca04c4fd
Nate Ensign <dixienate@gmail.com>
2025-03-01 09:45:34 -0700

    adjusted login and signup functions to generate a jwt

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,7 @@
 const { validationResult } = require("express-validator");
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -72,7 +73,21 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: createdUser.id, email: createdUser.emal },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Sign up failed, please try again.", 500);
+    return next(error);
+  }
+
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -100,13 +115,26 @@ const login = async (req, res, next) => {
   }
 
   if (!isValidPassword) {
-    const error = new HttpError('Invalid credentials, could not log in.', 401);
+    const error = new HttpError("Invalid credentials, could not log in.", 401);
+    return next(error);
+  }
+
+  let token;
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Login failed, please try again.", 500);
     return next(error);
   }
 
   res.json({
-    message: "Logged in!",
-    user: existingUser.toObject({ getters: true }),
+   userId: existingUser.id,
+   email: existingUser.email,
+   token: token
   });
 };
 
