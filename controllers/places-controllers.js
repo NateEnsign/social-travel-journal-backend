a342904339cf01d26e7895ee80d918655bbc2ed3
Nate Ensign <dixienate@gmail.com>
2025-03-02 13:22:01 -0700

    small update

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -80,7 +80,7 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId
   });
 
   let user;
